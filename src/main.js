1fcf7227e0481cc9f495a07b5adb99e1808fbb89
kbaba1001 <kbaba1001@users.noreply.github.com>
2019-05-31 21:51:15 +0900

    add sacctuary

--- src/main.js
+++ src/main.js
@@ -1,82 +1,99 @@
-import Maybe from './functional-types/maybe.js'
-import Either from './functional-types/either.js'
-import IO from './functional-types/io.js'
-import * as R from 'ramda'
+// import Maybe from './functional-types/maybe.js'
+// import Either from './functional-types/either.js'
+// import IO from './functional-types/io.js'
+// import * as R from 'ramda'
+
+import S from 'sanctuary'
+// import $ from 'sanctuary-def'
 
 export default function main() {
-  // data
-  const db = {
-    students: {
-      '444-44-4444': {
-        ssn: '444-44-4444',
-        firstname: 'Alonzo',
-        lastname: 'Church'
-      },
-      '444444444': {
-        ssn: '444-44-4444',
-        firstname: 'Alonzo',
-        lastname: 'Church'
-      },
-    },
-    find: function (ssn) {
-      return this.students[ssn]
-    }
-  }
+  // S.compose は2つの関数を合成するだけ
+  console.log(S.compose (Math.sqrt) (S.add (1)) (99))
 
-
-  const validLength = (len, str) => {
-    return str.length === len
-  }
-
-  const checkLengthSsn = ssn => {
-    return Either.of(ssn)
-      .filter(R.partial(validLength, [9]));
-  }
-  // const checkLengthSsn = (ssn) => {
-  //   return validLength(9, ssn) ? Either.right(ssn) : Either.left('Invalid SSN')
-  // }
-
-  const find = R.curry((db, id) => db.find(id))
-
-  const safeFindObject = R.curry((db, id) => Either.fromNullable(find(db, id)));
-  // const safeFindObject = R.curry((db, id) => {
-  //   const val = find(db, id)
-  //   return val ? Either.right(val) : Either.left(`Object not foundwith ID: ${id}`)
-  // })
-
-  const findStudent = safeFindObject(db)
-
-  const csv = (arr) => arr.join(',')
-
-  const trim = (str) => str.replace(/^\s*|\s*$/g, '')
-  const normalize = (str) => str.replace(/\-/g, '')
-  const cleanInput = R.compose(normalize, trim)
-
-  const append = function (elementId) {
-    return function (info) {
-      document.querySelector(elementId).innerHTML = info
-      return info
-    }
-  }
-
-  const map = R.curry((f, container) => container.map(f))
-  const chain = R.curry((f, container) => container.chain(f))
-  const lift = R.curry((f, obj) => Maybe.fromNullable(f(obj)))
-  const liftIO = (val) => IO.of(val)
-  const getOrElse = R.curry((message, container) => container.getOrElse(message))
-
-  const trace = R.curry((msg, obj) => console.log(msg, obj))
-
-  const showStudent = R.compose(
-    map(append('#student-info')),
-    liftIO,
-    getOrElse('unable to find student'),
-    map(csv),
-    map(R.props(['ssn', 'firstname', 'lastname'])),
-    chain(findStudent),
-    chain(checkLengthSsn),
-    lift(cleanInput)
+  const hoge = S.pipe([
+    S.add (1),
+    Math.sqrt,
+    S.sub (1)]
   )
+  console.log(hoge(99))
 
-  showStudent('444-44-4444').run()
+  // console.log(S.type (S.Just (42)))
 }
+
+// export default function main() {
+//   // data
+//   const db = {
+//     students: {
+//       '444-44-4444': {
+//         ssn: '444-44-4444',
+//         firstname: 'Alonzo',
+//         lastname: 'Church'
+//       },
+//       '444444444': {
+//         ssn: '444-44-4444',
+//         firstname: 'Alonzo',
+//         lastname: 'Church'
+//       },
+//     },
+//     find: function (ssn) {
+//       return this.students[ssn]
+//     }
+//   }
+
+
+//   const validLength = (len, str) => {
+//     return str.length === len
+//   }
+
+//   const checkLengthSsn = ssn => {
+//     return Either.of(ssn)
+//       .filter(R.partial(validLength, [9]));
+//   }
+//   // const checkLengthSsn = (ssn) => {
+//   //   return validLength(9, ssn) ? Either.right(ssn) : Either.left('Invalid SSN')
+//   // }
+
+//   const find = R.curry((db, id) => db.find(id))
+
+//   const safeFindObject = R.curry((db, id) => Either.fromNullable(find(db, id)));
+//   // const safeFindObject = R.curry((db, id) => {
+//   //   const val = find(db, id)
+//   //   return val ? Either.right(val) : Either.left(`Object not foundwith ID: ${id}`)
+//   // })
+
+//   const findStudent = safeFindObject(db)
+
+//   const csv = (arr) => arr.join(',')
+
+//   const trim = (str) => str.replace(/^\s*|\s*$/g, '')
+//   const normalize = (str) => str.replace(/\-/g, '')
+//   const cleanInput = R.compose(normalize, trim)
+
+//   const append = function (elementId) {
+//     return function (info) {
+//       document.querySelector(elementId).innerHTML = info
+//       return info
+//     }
+//   }
+
+//   const map = R.curry((f, container) => container.map(f))
+//   const chain = R.curry((f, container) => container.chain(f))
+//   const lift = R.curry((f, obj) => Maybe.fromNullable(f(obj)))
+//   const liftIO = (val) => IO.of(val)
+//   const getOrElse = R.curry((message, container) => container.getOrElse(message))
+
+//   const trace = R.curry((msg, obj) => console.log(msg, obj))
+
+//   const showStudent = R.compose(
+//     map(append('#student-info')),
+//     liftIO,
+//     getOrElse('unable to find student'),
+//     map(csv),
+//     map(R.props(['ssn', 'firstname', 'lastname'])),
+//     chain(findStudent),
+//     chain(checkLengthSsn),
+//     lift(cleanInput)
+//   )
+
+//   showStudent('444-44-4444').run()
+// }
